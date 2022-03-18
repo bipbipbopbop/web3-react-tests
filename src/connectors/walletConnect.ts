a4baf50ea2632132c9c2d2c930d0c68263b8b204
Julien Hache <julien.hache7@gmail.com>
2022-03-18 15:09:59 +0100

    Add polygon network for WalletConnect connector.

--- src/connectors/walletConnect.ts
+++ src/connectors/walletConnect.ts
@@ -1,21 +1,28 @@
 import { initializeConnector } from "@web3-react/core";
 import { WalletConnect } from "@web3-react/walletconnect";
-// import type { AddEthereumChainParameter } from "@web3-react/types";
+import type { AddEthereumChainParameter } from "@web3-react/types";
 
 interface BasicChainInformation {
   urls: string[];
   name: string;
 }
 
-// interface ExtendedChainInformation extends BasicChainInformation {
-//   nativeCurrency: AddEthereumChainParameter["nativeCurrency"];
-//   blockExplorerUrls: AddEthereumChainParameter["blockExplorerUrls"];
-// }
+interface ExtendedChainInformation extends BasicChainInformation {
+  nativeCurrency: AddEthereumChainParameter["nativeCurrency"];
+  blockExplorerUrls: AddEthereumChainParameter["blockExplorerUrls"];
+}
+
+const MATIC: AddEthereumChainParameter["nativeCurrency"] = {
+  name: "Matic",
+  symbol: "MATIC",
+  decimals: 18,
+};
 
 // list of all compatible chains
 const CHAINS: {
-  [chainId: number]: BasicChainInformation; // | ExtendedChainInformation;
+  [chainId: number]: BasicChainInformation | ExtendedChainInformation;
 } = {
+  // Ethereum
   1: {
     urls: [
       process.env.infuraKey
@@ -27,6 +34,18 @@ const CHAINS: {
       "https://cloudflare-eth.com",
     ].filter((url) => url !== undefined) as string[],
     name: "Mainnet",
+  },
+  // Polygon
+  137: {
+    urls: [
+      process.env.infuraKey
+        ? `https://polygon-mainnet.infura.io/v3/${process.env.infuraKey}`
+        : undefined,
+      "https://polygon-rpc.com",
+    ].filter((url) => url !== undefined) as string[],
+    name: "Polygon Mainnet",
+    nativeCurrency: MATIC,
+    blockExplorerUrls: ["https://polygonscan.com"],
   },
 };
 
